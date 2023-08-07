8b046b5a0d77ac87195155e23f4c1b8d72fc74fd
matth1290 <matth1290@berkeley.edu>
2023-08-06 23:28:19 -0700

    deploy

--- src/App.js
+++ src/App.js
@@ -1,14 +1,71 @@
-import React from 'react';
+// App.js
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import HorizontalScrollGallery from './HorizontalScrollGallery/HorizontalScrollGallery';
+import LoginPage from './LoginPage/LoginPage';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 const App = () => {
   // Replace this array with the names of your art assets.
   const artList = ['aegina_sunset.jpeg', 'balloon_day.jpeg', 'berkeley_fiery_sky.jpeg', 'morro_bay.jpeg', 'friendly_giraffe.jpeg', 'little_boat.jpeg', 'pv_candy_sky.jpeg'];
 
+  // State to keep track of user authentication status
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthChecked, setIsAuthChecked] = useState(false);
+
+  useEffect(() => {
+    // Function to handle changes in the user's authentication state
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, set isAuthenticated to true
+        setIsAuthenticated(true);
+      } else {
+        // User is signed out, set isAuthenticated to false
+        setIsAuthenticated(false);
+      }
+      // Mark authentication check as finished
+      setIsAuthChecked(true);
+    });
+
+    // Clean up the listener when the component unmounts
+    return () => unsubscribe();
+  }, []);
+
+  // Function to handle successful login
+  const handleLogin = () => {
+    setIsAuthenticated(true);
+  };
+
+  // Render a loading state while the authentication check is in progress
+  if (!isAuthChecked) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="App">
-      <HorizontalScrollGallery artList={artList} />
+      <Router>
+        <Routes>
+          {/* Redirect to login page if not authenticated */}
+          <Route
+            path="/"
+            element={
+              isAuthenticated ? <Navigate to="/gallery" /> : <Navigate to="/login" />
+            }
+          />
+
+          {/* Public routes */}
+          <Route path="/login" element={<LoginPage onLogin={handleLogin} />} />
+
+          {/* Protected routes */}
+          {isAuthenticated ? (
+            <Route path="/gallery" element={<HorizontalScrollGallery artList={artList} />} />
+          ) : (
+            <Route path="/gallery" element={<Navigate to="/login" />} />
+          )}
+        </Routes>
+      </Router>
     </div>
   );
 };
--- src/HorizontalScrollGallery/HorizontalScrollGallery.js
+++ src/HorizontalScrollGallery/HorizontalScrollGallery.js
@@ -1,13 +1,17 @@
+// HorizontalScrollGallery.js
 import React, { useEffect, useState } from 'react';
 import './HorizontalScrollGallery.css';
 import { getFirestore, collection, getDocs } from 'firebase/firestore';
-import { getStorage, ref, getDownloadURL } from 'firebase/storage';
+import { getAuth, signOut } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 import ImageWithMetadata from './ImageWithMetadata';
 import ImageUploadForm from './ImageUploadForm';
 
 const HorizontalScrollGallery = () => {
+  const navigate = useNavigate();
   const [artList, setArtList] = useState([]);
   const [showForm, setShowForm] = useState(false); // State to control form visibility
+  const [userEmail, setUserEmail] = useState('');
 
   const fetchImageMetadata = async () => {
     try {
@@ -26,6 +30,12 @@ const HorizontalScrollGallery = () => {
 
   useEffect(() => {
     fetchImageMetadata();
+
+    // Get the user's email after authentication
+    const auth = getAuth();
+    if (auth.currentUser) {
+      setUserEmail(auth.currentUser.email);
+    }
   }, []);
 
   const handleAddToGallery = () => {
@@ -38,9 +48,23 @@ const HorizontalScrollGallery = () => {
     setShowForm(false);
   };
 
+  const handleSignOut = async () => {
+    try {
+      await signOut(getAuth());
+      navigate('/');
+    } catch (error) {
+      console.error('Error signing out:', error);
+    }
+  };
+
   return (
-    <div>
-    <h1 className='big-message'>Welcome to Matt's Art Gallery</h1>
+    <div className="horizontal-scroll-gallery">
+      <div className="top-right">
+        <button className="leave-btn" onClick={handleSignOut}>
+          Leave Gallery
+        </button>
+      </div>
+      <h1 className="big-message">Welcome to Matt's Art Gallery</h1>
       <div className="horizontal-scroll-container">
         {artList.map((art, index) => (
           <div key={index} className="art-item">
@@ -48,9 +72,11 @@ const HorizontalScrollGallery = () => {
           </div>
         ))}
       </div>
-      <button className="add-to-gallery-btn" onClick={handleAddToGallery}>
-        Add to Gallery
-      </button>
+      {userEmail === 'matth1290@berkeley.edu' && (
+        <button className="add-to-gallery-btn" onClick={handleAddToGallery}>
+          Add to Gallery
+        </button>
+      )}
       {showForm && <ImageUploadForm onClose={handleCloseForm} />}
     </div>
   );
